7e7c0b79ea8aa2c9f32a7380c912638617e8f3a9
phucbienvan <phuc.bienvan@gmail.com>
2022-10-19 23:02:02 +0700

    add func

--- contracts/bep20/SampleToken.sol
+++ contracts/bep20/SampleToken.sol
@@ -3,5 +3,55 @@ pragma solidity ^0.8.4;
 import "./IERC20.sol";
 
 contract SampleToken is IERC20 {
+    constructor () {
+        _totalSupply = 1000000;
+        _balances[msg.sender] = 1000000;
+    }
 
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    function totalSupply() public view override returns (uint256)
+    {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256 balance)
+    {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool success)
+    {
+        require(_balances[msg.sender] >= amount);
+        _balances[msg.sender] -= amount;
+        _balances[recipient] += amount;
+        emit Trasfer(msg.sender, recipient, amount);
+
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool success)
+    {
+        require(_blances[sender] >= amount);
+        require(_allowances[sender][msg.sender] >= amount);
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool success)
+    {
+        _allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+
+        return true;
+    }
+    function allowance(address owner, address spender) public view returns (uint256 remaining)
+    {
+        return _allowances[owner][spender];
+    }
 }
